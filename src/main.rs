a51bc4f83b78ad21ea997e76e72e93abbfcb73d5
Spencer Bartholomew <38776747+spencerbart@users.noreply.github.com>
2024-07-11 10:33:48 -0600

    multiplex

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::{net::SocketAddr, sync::Arc};
 
+use axum::{extract::Request, http::header::CONTENT_TYPE, routing::get, Router};
 use backend::{services::deck::deck_service, AppContext};
 use dashmap::DashMap;
 use tonic::transport::Server as TonicServer;
+use tower::{make::Shared, steer::Steer};
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+async fn web_root() -> &'static str {
+    tracing::info!("Got a REST request");
+
+    "Hello, World!"
+}
 
 #[tokio::main]
 async fn main() {
@@ -28,12 +36,28 @@ async fn main() {
         .layer(TraceLayer::new_for_grpc())
         .into_inner();
 
-    tracing::info!("Listening on {}", addr);
-
-    TonicServer::builder()
+    let grpc = TonicServer::builder()
         .layer(layer)
         .add_service(deck_svc)
-        .serve(addr)
-        .await
-        .unwrap();
+        .into_router();
+
+    let rest = Router::new().route("/", get(web_root));
+    let service = Steer::new(vec![rest, grpc], |req: &Request, _services: &[_]| {
+        if req
+            .headers()
+            .get(CONTENT_TYPE)
+            .map(|content_type| content_type.as_bytes())
+            .filter(|content_type| content_type.starts_with(b"application/grpc"))
+            .is_some()
+        {
+            1
+        } else {
+            0
+        }
+    });
+
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    tracing::debug!("listening on {}", addr);
+    axum::serve(listener, Shared::new(service)).await.unwrap();
 }
